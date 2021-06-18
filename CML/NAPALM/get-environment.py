c1800c1a15356457ef9663e2148deb49aaace5ab
tejanke <62154969+tejanke@users.noreply.github.com>
2021-06-17 21:58:36 -0400

    inventory function

--- CML/NAPALM/get-environment.py
+++ CML/NAPALM/get-environment.py
@@ -1,4 +1,5 @@
 # https://napalm.readthedocs.io/en/latest/support/index.html
+# https://buildmedia.readthedocs.org/media/pdf/napalm/latest/napalm.pdf
 
 import napalm
 import sys
@@ -7,30 +8,40 @@ import pprint
 import yaml
 
 inventory_file = "inventory.txt"
-main_inventory = {}
 
-""" driver = napalm.get_network_driver("ios")
-device = driver(hostname="10.10.20.181", username="cisco", password="cisco")
-device.open()
-device_environment = device.get_environment()
-device_interfaces = device.get_interfaces()
-device_arp_table = device.get_arp_table()
-device.close()
-print(device_environment)
-print(device_interfaces)
-print(device_arp_table) """
+def get_inventory(inventory_file):
+    """Create inventory"""
+    inventory = {}
+    with open(inventory_file, 'r') as file:
+        inventory_counter = 0
+        for line in file:
+            inventory_item = line.rstrip().replace(' ','').split(',')
+            inventory[inventory_counter] = {
+                    "hostname" : inventory_item[0],
+                    "driver" : inventory_item[1],
+                    "ip" : inventory_item[2],
+                    "username" : inventory_item[3],
+                    "password" : inventory_item[4]
+                }
+            inventory_counter += 1
+    return inventory
 
-with open(inventory_file, 'r') as file:
-    inventory_counter = 0
-    for line in file:
-        inventory_item = line.rstrip().replace(' ','').split(',')
-        main_inventory[inventory_counter] = {
-            "hostname" : inventory_item[0],
-            "driver" : inventory_item[1],
-            "ip" : inventory_item[2],
-            "username" : inventory_item[3],
-            "password" : inventory_item[4]
-        }
-        inventory_counter += 1
+def get_environment(device_number, device):
+    """Grab environment variables"""
+    print("Opening a connection to {} at {}.".format(device['hostname'], \
+        device['ip']))
+    driver = napalm.get_network_driver(device['driver'])
+    if device['driver'] != 'asa':
+        with driver(hostname = device['ip'], username = device['username'], \
+            password = device['password'], optional_args = \
+            {'secret' : 'cisco'}) as conn:
+                device_environment = conn.get_environment()
+    else:
+        print("\tget_environment not supported")
+        device_environment = {}
+    return device_environment
 
-print(yaml.dump(main_inventory))
+main_inventory = get_inventory(inventory_file)
+for device_number, device in main_inventory.items():
+    device_environment = get_environment(device_number, device)
+    print(yaml.dump(device_environment))
